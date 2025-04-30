bdc312d5317f5c78a37273f333bc48fee18c7b86
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-04-30 12:31:49 +0600

    Project6 commit

---
+++ JavaLab.java
@@ -0,0 +1,8 @@
+public class JavaLab {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a Java lab.");
+        System.out.println("Let's learn Java together.");
+        System.out.println("Java is a versatile programming language.");
+    }
+}
